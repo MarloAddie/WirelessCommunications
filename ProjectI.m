dad6ec8267ee93bfe2541910d159541f5ec9fa0c
MarloAddie <marlo.esperson@gmail.com>
2024-02-07 11:09:45 -0700

    Update ProjectI.m

--- ProjectI.m
+++ ProjectI.m
@@ -9,3 +9,11 @@ close all
 % Generate a sequence of N*M QPSK information symbols d~ in the frequency
 % domain, where M = 16  is the number of subcarriers and N = 4 is the
 % number of OFDM symbols.
+
+M = 16;           % # of subcarriers
+N = 4;            % # of OFDM symbols
+numSymbols = N*M; % # of QPSK info symbols
+
+% Generate rand binary data
+randSeq = rand(1, numSymbols);
+binaryData = (randSeq > 0.5);
