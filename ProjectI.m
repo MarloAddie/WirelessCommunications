d2cff82f3c000a6508610eb1d1119152adca48d5
fuzzybear789 <81193854+fuzzybear789@users.noreply.github.com>
2024-02-07 13:59:04 -0700

    Update ProjectI.m

--- ProjectI.m
+++ ProjectI.m
@@ -1,6 +1,3 @@
-% ECE 6961 - Project Part I
-% Emma Dingman, Collin Griswold, Marlo Esperson
-
 clc
 clear all
 close all
@@ -9,3 +6,16 @@ close all
 % Generate a sequence of N*M QPSK information symbols d~ in the frequency
 % domain, where M = 16  is the number of subcarriers and N = 4 is the
 % number of OFDM symbols.
+
+M = 16;           % # of subcarriers
+N = 1;            % # of OFDM symbols
+numSymbols = N*M; % # of QPSK info symbols
+
+% Generate rand binary data
+randSeq = rand(1, numSymbols);
+binaryData = (randSeq > 0.5);
+x = zeros(1, length(randSeq*4));
+cp = 5;
+
+x(1:4) = binaryData(13:16);
+x(5:20) = binaryData(1:16);
