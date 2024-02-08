4185fb69a2443fd68f5b80b675d5b843ee60a033
MarloAddie <marlo.esperson@gmail.com>
2024-02-08 00:19:03 -0700

    Update ProjectI.m

--- ProjectI.m
+++ ProjectI.m
@@ -17,5 +17,33 @@ numSymbols = N*M; % # of QPSK info symbols
 % Generate rand binary data
 randSeq = rand(1, numSymbols);
 binaryData = (randSeq > 0.5);
+s = zeros(1, length(randSeq*4)); % Symbols
+cp = 5;
 
-% Hello
+s(1:4) = binaryData(13:16);
+s(5:20) = binaryData(1:16);
+
+realComp = s(1:2:end);
+imagComp = s(2:2:end);
+
+graycode = zeros(1,length(realComp));
+
+for i = 1:length(realComp)
+    if realComp(i) && imagComp(i) == 0
+        graycode(i) = (1/sqrt(2))*(1+1i);
+    end
+    
+    if realComp(i) && imagComp(i) == 1
+        graycode(i) = (1/sqrt(2))*(1-1i);
+    end
+    
+    if realComp(i) == 0 && imagComp(i) == 1
+        graycode(i) = (1/sqrt(2))*(-1+1i);
+    end
+    
+    if realComp(i) == 1 && imagComp(i) == 0
+        graycode(i) = (1/sqrt(2))*(-1-1i);
+    end
+end
+
+
