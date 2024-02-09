ba62a679a7629d51e668a3ce5e0654301fb32012
fuzzybear789 <81193854+fuzzybear789@users.noreply.github.com>
2024-02-09 16:33:42 -0700

    Update ProjectI.m

--- ProjectI.m
+++ ProjectI.m
@@ -14,7 +14,9 @@ binaryData = (randSeq > 0.5);
 
 
 
-h_k = [1,2,3,4,5];
+real = binaryData(1:2:end);
+imag = binaryData(2:2:end);
+
 
 % Create indexing for channel matrix based on knowlegde of signal
 x = zeros(1, M);
@@ -26,11 +28,8 @@ new_x = zeros(1, length(x)+cp);
 new_x(1:cp) = x((end-cp+1):end);
 new_x(cp+1:end) = x(1:end);
 
-
-
 % QPSK mapping
-real = binaryData(1:2:end);
-imag = binaryData(2:2:end);
+d_tilde = zeros(length(real),1);
 for i = 1:length(real)
     if (real(i) == imag(i))
         re = 1;
@@ -42,13 +41,13 @@ for i = 1:length(real)
     else
         im = -1i;
     end
-    
-    s(i) = 1/sqrt(2)*(re+im);
+    d_tilde(i) = 1/sqrt(2)*(re+im);
 end
 
+h_k = [0.227,0.46,0.688,0.46,0.227];
 % channel modeling designed for a signal with a cyclic prefix
 h = zeros(M,M); % 16x16 matrix for the modeled channel
-for i = cp+2:M+cp+1 % start after the cyclic prefix
+for i = cp+2:M+cp+1 % start after the cyclic prefix (adds and removes cp in one step)
     for k = 1:L % go through the taps
         jk = new_x(i - k);  
             h(i-5,jk) = h_k(k);
@@ -63,16 +62,30 @@ for i = 1:M
     end
 end
 
-
-ss = s(1:16:end); % Seperate the signal by each symbol
-Y = zeros(64,16);
-for sub = 1:4
-
 F = (1/sqrt(M)) .* f_i_j; % F_N matrix
 
-x = F'.*ss(1); % transmitted signal
+F_I = F'*F;
 
-y = h*x; % received signal (time domain)
-jklm = sub+(sub-1)*(16-1);
-Y(jklm:jklm+15,1:M) = F*y; % received signal (freq domain)
+
+
+
+
+y_tilda = zeros(64,16);
+for sub = 1:4
+jklm = sub+(sub-1)*(16-1); % weird indexing shoudl fix
+dd = d_tilde(jklm:jklm+15,1);
+d = F'*dd; % transmitted signal
+y = h*d; % received signal (time domain)
+y_tilda(jklm:jklm+15,1) = F*y; % received signal (freq domain)
+
 end
+
+y_tilde = y_tilda(1:16,1);
+
+% verify correctness using one-tap equalizer:
+h_tilde = (F*h*F'); % freq domain channel
+h_tilde_diag = diag(h_tilde,0); % one tap equalizer (diagonal of h_tilde)
+d_tilde_rx = y_tilde./h_tilde_diag; % received symbols (equal to d_tilde)
+
+
+
