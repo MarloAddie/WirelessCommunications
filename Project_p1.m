24c611c2c71f44fd116995f473f061cd25ff1d93
Collin Griswold <124535504+collingriswold@users.noreply.github.com>
2024-02-14 12:27:16 -0700

    Update Project_p1.m

--- Project_p1.m
+++ Project_p1.m
@@ -1,91 +1,169 @@
-% ECE 6961 - Project Part I
+%% ECE 6961 - Project Part I
 % Emma Dingman, Collin Griswold, Marlo Esperson
+
+%% CP-OFDM
 
 clear all, clc, close all
 
-% Task 1: Understand CP-OFDM and ZP-OFDM
-% Generate a sequence of N*M QPSK information symbols d~ in the frequency
-% domain, where M = 16  is the number of subcarriers and N = 4 is the
-% number of OFDM symbols.
+L = 5; % # of taps
+M = 16; % # of subcarriers
+N = 4; % # of OFDM symbols
 
-M = 16;           % # of subcarriers
-N = 4;            % # of OFDM symbols
-numSymbols = N*M; % # of QPSK info symbols
+% initialize outputs
+y_tilde = [];
+d_tilde = [];
+d_tilde_rx = [];
 
-% create bits and map them to QPSK symbols:
-num=32;
-b=sign(randn(num,1));
-b = (b+1)/2;
-b = reshape(b,num/2,2);
+for n = 1:N
+    [new_y_tilde,new_d_tilde,new_d_tilde_rx] = CP_OFDM(L,M);
+    y_tilde = cat(1,new_y_tilde, y_tilde); % received signal for N OFDM symbols
+    d_tilde = cat(1,new_d_tilde, d_tilde); % M*N transmitted QPSK symbols
+    d_tilde_rx = cat(1,new_d_tilde_rx, d_tilde_rx); % M*N received QPSK symbols (should match d_tilde)
+end
 
-for n = 1:length(b)
-    if b(n,1) == 0
-        imaginary(n,1) = 1;
-        if b(n,2) == 0
-            real(n,1) = 1;
+%% ZP-OFDM
+
+clear all, clc, close all
+
+L = 5; % # of taps
+M = 16; % # of subcarriers
+N = 4; % # of OFDM symbols
+
+% initialize outputs
+y_tilde = [];
+d_tilde = [];
+d_tilde_rx = [];
+
+for n = 1:N
+    [new_y_tilde,new_d_tilde,new_d_tilde_rx] = ZP_OFDM(L,M);
+    y_tilde = cat(1,new_y_tilde, y_tilde); % received signal for N OFDM symbols
+    d_tilde = cat(1,new_d_tilde, d_tilde); % M*N transmitted QPSK symbols
+    d_tilde_rx = cat(1,new_d_tilde_rx, d_tilde_rx); % M*N received QPSK symbols (should match d_tilde)
+end
+
+
+
+
+
+
+
+
+
+
+
+
+%% Functions:
+
+% CP-OFDM:
+function [y_tilde,d_tilde,d_tilde_rx] = CP_OFDM(L,M)
+    cp = L-1; % length of cp
+    numBits = M*2; % # of bits to generate M QPSK symbols
+    
+    % Generate random binary data
+    binaryData = randi([0 1], numBits, 1);
+    
+    real = binaryData(1:2:end);
+    imag = binaryData(2:2:end);
+    s = [real imag];
+    
+    % QPSK mapping
+    d_tilde = zeros(length(real),1);
+    for i = 1:length(real)
+        if (real(i) == imag(i))
+            re = 1;
         else
-            real(n,1) = -1;
+            re = -1;
         end
-    else
-        imaginary(n,1) = -1;
-        if b(n,2) == 0
-            real(n,1) = -1;
+        if (real(i) < 1)
+            im = 1i;
         else
-            real(n,1) = 1;
+            im = -1i;
         end
+        d_tilde(i) = 1/sqrt(2)*(re+im); % QPSK symbols
     end
-
-    d_tilde(n,1) = real(n,1) + 1j*imaginary(n,1);
-end
-d_tilde = (1/sqrt(2)).*d_tilde; % symbols
-
-% taps:
-h0 = 0.227;
-h1 = 0.46;
-h2 = 0.688;
-h3 = 0.46;
-h4 = 0.227;
-
-% create channel:
-h = [h0 0 0 0 0 0 0 0 0 0 0 0 h4 h3 h2 h1]; % first line (y5)
-for n = 2:16
-    h(n,:) = circshift(h(n-1,:),1); % channel matrix
-end
-
-% generate F_N matrix:
-for i = 1:M
-    for J = 1:M
-        f_i_j(i,J) = exp((-1j*2*pi*(i-1)*(J-1))/M);
+    
+    
+    d = ifft(d_tilde); % time-domain symbols
+    
+    x = [d(end-cp+1:end);d(:)]; % add cp
+    
+    h = [0.227 0.46 0.688 0.46 0.227]; % taps
+    
+    y = zeros(M,1);
+    for m = L:M+L-1 % remove cp
+        y_m = 0;
+       for l = 0:L-1
+            y_m = h(l+1)*x(m-l) + y_m; % convolution
+       end
+       y(m-L+1) = y_m; % received signals (time-domain)
     end
+    
+    y_tilde = fft(y); % received signals (freq-domain)
+    
+    % verify correctness using one-tap equalizer:
+    h_tilde = fft(h,M);
+    h_tilde = transpose(h_tilde); % channel in freq-domain
+    d_tilde_rx = y_tilde./h_tilde; % received symbols are equal to transmitted QPSK symbols
+
 end
-F = (1/sqrt(M)) .* f_i_j; % F_N matrix
 
-d = F'*d_tilde; % transmitted signal (IDFT)
-y = h*d; % received signal (time domain)
-y_tilde= F*y; % received signal (DFT)
+% ZP-OFDM:
+function [y_tilde,d_tilde,d_tilde_rx] = ZP_OFDM(L,M)
+    
+    numBits = M*2; % # of bits to generate M QPSK symbols
+    
+    % Generate random binary data
+    binaryData = randi([0 1], numBits, 1);
+    
+    real = binaryData(1:2:end);
+    imag = binaryData(2:2:end);
+    s = [real imag];
+    
+    % QPSK mapping
+    d_tilde = zeros(length(real),1);
+    for i = 1:length(real)
+        if (real(i) == imag(i))
+            re = 1;
+        else
+            re = -1;
+        end
+        if (real(i) < 1)
+            im = 1i;
+        else
+            im = -1i;
+        end
+        d_tilde(i) = 1/sqrt(2)*(re+im); % QPSK symbols
+    end
+    
+    d = ifft(d_tilde); % time-domain symbols
+    
+    x = [zeros(L-1,1);d(:);zeros(L-1,1)]; % pad with zeros
+    
+    h = [0.227 0.46 0.688 0.46 0.227]; % taps
+    
+    y = zeros(M,1);
+    for m = L:M+2*(L-1) 
+        y_m = 0;
+       for l = 0:L-1
+            y_m = h(l+1)*x(m-l) + y_m; % convolution
+       end
+       y(m-L+1) = y_m; % received signals (time-domain)
+    end
+    
+    y_tilde = zeros(M,1);
+    for i = 0:M-1
+        y_i = 0;
+        for k = 0:M+L-2
+            y_i = y(k+1)*exp((-1j*2*pi*k*i)/M) + y_i; % modified DFT
+        end
+        y_tilde(i+1) = y_i; % received signals (freq-domain)
+    end
+    
+    
+    % verify correctness using one-tap equalizer:
+    h_tilde = fft(h,M);
+    h_tilde = transpose(h_tilde); % channel in freq-domain
+    d_tilde_rx = y_tilde./h_tilde; % received symbols are equal to transmitted QPSK symbols
 
+end
 
-% verify correctness using one-tap equalizer:
-h_tilde = (F*h*F'); % freq domain channel
-h_tilde_diag = diag(h_tilde); % one tap equalizer (diagonal of h_tilde)
-d_tilde_rx = y_tilde./h_tilde_diag; % received symbols (equal to d_tilde)
-
-
-
-% alternate way to get y_tilde:
-% h_tilde = (F*h*F');
-% y_tilde = d_tilde.*h_tilde;
-% y_tilde = diag(y_tilde,0);
-
-
-
-
-
-
-% TESTING:
-% F_I = F'*F; % identity matrix
-% 
-% h_f= F*h*F'; % channel in freq domain
-
-% figure; [ch,w]=freqz([h0 h1 h2 h3 h4],1,'whole',2001);
-% plot(w/pi/2, 20*log10(abs(ch)));
