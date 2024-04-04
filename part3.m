51aa0403b16fff0c34da4aa77899241c265c4eb2
fuzzybear789 <81193854+fuzzybear789@users.noreply.github.com>
2024-04-04 13:52:03 -0600

    Update part3.m

--- part3.m
+++ part3.m
@@ -3,6 +3,7 @@ clc, clear, close all
 load('zpw.mat')
 load('OFDM_PILOT.mat')
 load('ofdm_map.mat')
+load('z_w.mat')
 
 k = 2048;
 L = 199;
@@ -23,7 +24,7 @@ D = OFDM_PILOT(ofdm_map==1);
 D = diag(D);
 hls = (1/kp)*V'*D'*Z_pw;
 
-for nn = 1:kp
+for nn = 1:k
     for n = 1:L+1
         V2(nn,n) = exp((-1i*2*pi*(n-1)*(nn-1))/k);
     end
@@ -31,6 +32,37 @@ end
 
 Hw = V2*hls;
 
-Z_pw_null = Z_pw(ofdm_map ==0);
+Z_pw_null = z_w((ofdm_map == 0));
 
-noiseVarience = (1/null_sub);
+noiseVar = (1/null_sub)*sum(abs(Z_pw_null).^2);
+
+sd = find(ofdm_map == 2);
+
+zd = z_w(sd-1);
+
+HD = Hw(sd-1);
+
+X1 = 1/sqrt(2) + (1/sqrt(2))*1i;
+X2 = -1/sqrt(2) + (1/sqrt(2))*1i;
+X3 = 1/sqrt(2) - (1/sqrt(2))*1i;
+X4 = -1/sqrt(2) - (1/sqrt(2))*1i;
+
+LR = [];
+for pl = 1:length(HD)
+    
+    Lb1 = log10((exp(-abs(zd(pl)-HD(pl)*X1)^2/noiseVar)+exp(-abs(zd(pl)-HD(pl)*X3)^2/noiseVar))...
+        /(exp(-abs(zd(pl)-HD(pl)*X2)^2/noiseVar)+exp(-abs(zd(pl)-HD(pl)*X4)^2/noiseVar)));
+    
+    Lb2 = log10((exp(-abs(zd(pl)-HD(pl)*X1)^2/noiseVar)+exp(-abs(zd(pl)-HD(pl)*X2)^2/noiseVar))...
+        /(exp(-abs(zd(pl)-HD(pl)*X3)^2/noiseVar)+exp(-abs(zd(pl)-HD(pl)*X4)^2/noiseVar)));
+    
+    LR1 = cat(1, Lb1, Lb2);
+    
+    LR = cat(1, LR, LR1);
+    
+end
+
+
+
+
+
