93f95fc1ca3d4db8885e06f283b8b7a704d5e151
MarloAddie <54915196+MarloAddie@users.noreply.github.com>
2024-04-15 12:11:40 -0600

    Add files via upload

--- part3_2.m
+++ part3_2.m
@@ -8,17 +8,19 @@ load('ofdm_map.mat')
 %%
 load('benchmark_NoiseVar_172648_1.mat')
 load('benchmark_Zw_172648_1.mat')
+load('INTRLVR.mat')
+load('CODE.mat')
 %%
 
 k = 2048;
 L = 199;
-kp = 512;
+kp = 512; % # of Subcarriers
 null_sub = 112;
 
 idx = find(ofdm_map ==1);
 
 z_w = bb_rece_data_172648_1474;
-Z_pw = z_w(ofdm_map==1);
+Z_pw = z_w(ofdm_map==1); % Symbols after doppler compensation
 
 for nn = 1:kp
     for n = 1:L+1
@@ -28,16 +30,17 @@ end
 
 D = OFDM_PILOT(ofdm_map==1);
 
-D = diag(D);
-hls = (1/kp)*V'*D'*Z_pw;
+D = diag(D); % Transmitted QPSK symbols
+hls = (1/kp)*V'*D'*Z_pw; % Time domain channel est  200 x 1
 
 for nn = 1:k
     for n = 1:L+1
-        V2(nn,n) = exp((-1i*2*pi*(n-1)*(nn-1))/k);
-    end
+        V2(nn,n) = exp((-1i*2*pi*(n-1)*(nn-1))/k); % Fourier transform 
+    end                                         % Dimension: (k x (L+1))
+                                                % 2048 x (200)
 end
 
-Hw = V2*hls;
+Hw = V2*hls; % Channel Gain. Freq domain. Dimension: k x (L+1)   (2048 x 200)
 
 Z_pw_null = z_w((ofdm_map == 0));
 
@@ -113,6 +116,15 @@ end
 NAME = './5G_LDPC_M10_N20_Z142_Q2_nonVer.txt';
 [address, LDPC_INFOLEN] = ldpc_mex_initial_CAPI([1420,2840,2],NAME);
 
+
+LR_in_de = zeros(length(LR),1);
+LR_in_de(INTRLVR) = LR;
+APP_code = ldpcDecoder_CAPI(address,LR_in_de);
+
+% Hard Decision
+
+est_code = (APP_code<0);
+bec = sum(abs(est_code-CODE(:,1)));
 
 
 
